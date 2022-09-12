42ff5f0e56a952957d1ece6d9340ef403cc2cdba
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-09-12 20:42:52 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,32 @@
+package Collegeinformation;
+
+public class details {
+    public static void main(String[]args){
+        details roll =  new details();
+         roll.CollegeName();
+         roll.StaffId();
+         roll.StaffName();
+         roll.StaffDept();
+
+    }
+    public void CollegeName(){
+        String CollegeName = "SRM University";
+        System.out.println(CollegeName);
+    }
+    public void StaffId(){
+        String StaffId = "18oibm009";
+        System.out.println(StaffId);
+    }
+    public void StaffName(){
+        String StaffName = "subhathra";
+        System.out.println(StaffName);
+    }
+    public void StaffDept(){
+        String StaffDept = "Computer Science";
+        System.out.println(StaffDept);
+    }
+    public void StudentName(){
+
+    }
+}
+
