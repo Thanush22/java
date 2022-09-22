88d897c3c4bfcc52292f36865bba333b82a9b016
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-09-22 23:02:37 +0530

    Update scanCls.java

--- Collegeinformation/scanCls.java
+++ Collegeinformation/scanCls.java
@@ -13,20 +13,22 @@ public class scanCls {
         System.out.println("Enter the name : ");
         String S1 = input.nextLine();
         System.out.println("Enter your SurName :");
-        String s9 = input.nextLine();
+        String s2 = input.nextLine();
+        System.out.println("Enter your RegisterNumber :");
+        String S3 = input.nextline();
         System.out.println("Enter the age : ");
-        String S2 = input.nextLine();
-        System.out.println("Enter your gender :");
-        String S3 = input.nextLine();
-        System.out.println("Enter your collegeName :");
         String S4 = input.nextLine();
-        System.out.println("Enter your Native :");
-        String S8 = input.nextLine();
-        System.out.println("Enter your FatherName :");
+        System.out.println("Enter your gender :");
         String S5 = input.nextLine();
-        System.out.println("Enter your motherName :");
+        System.out.println("Enter your collegeName :");
         String S6 = input.nextLine();
-        System.out.println("Enter your PhoneNumber :");
+        System.out.println("Enter your Native :");
         String S7 = input.nextLine();
+        System.out.println("Enter your FatherName :");
+        String S8 = input.nextLine();
+        System.out.println("Enter your motherName :");
+        String S9 = input.nextLine();
+        System.out.println("Enter your PhoneNumber :");
+        String S0 = input.nextLine();
     }
 }
