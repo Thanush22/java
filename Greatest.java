c278ba036a0cb07a583f7c74113f2d0880d8b512
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-10-27 22:49:02 +0530

    Add files via upload

---
+++ Greatest.java
@@ -0,0 +1,18 @@
+package type;
+
+public class Greatest {
+    public static void main(String []args){
+        int a = 45;
+        int b = 44;
+        int c = 19;
+        if(a<b){
+            System.out.println(b);
+        }
+        else if (b<c) {
+            System.out.println(c);
+        }
+        else{
+            System.out.println(a);
+            }
+    }
+}
