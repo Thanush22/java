8a9e6684518a175372c00d3bed55a407f974aa5e
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-10-30 21:41:05 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,13 @@
+package type;
+
+public class check {
+    public static void main(String[]args){
+        int i =-22;
+        if(i>0){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("negative");
+        }
+    }
+}
