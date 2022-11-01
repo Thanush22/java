06a725129dffc70214c1232e7207986024b3dfb9
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-11-01 22:30:14 +0530

    Add files via upload

---
+++ ascendingorder.java
@@ -0,0 +1,12 @@
+package type;
+
+public class ascendingorder {
+    public static void main(String[]args){
+        int a[]={22,0,11,55,33};
+        for(int i=1;i<a.length;i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.println();
+            }
+        }
+    }
+}
