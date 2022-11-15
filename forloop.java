479eb5b97ad2389479b30dfed59dac6bf02928fd
Thanush22 <95023151+Thanush22@users.noreply.github.com>
2022-11-15 23:57:31 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,17 @@
+package type;
+
+import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int a;
+        a = scan.nextInt();
+        for (a = 0; a<1; a++) {
+            System.out.println("");
+        }
+
+
+    }
+    }
